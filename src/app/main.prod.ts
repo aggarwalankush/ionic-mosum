85b12da782d15ecd531bf26afd7207628ac5d657
Ankush Aggarwal <ankushagg93@gmail.com>
2016-12-14 12:53:02 -0800

    updated @ionic/app-scripts to 0.0.47

--- src/app/main.prod.ts
+++
@@ -1,7 +0,0 @@
-import {platformBrowser} from "@angular/platform-browser";
-import {enableProdMode} from "@angular/core";
-//noinspection TypeScriptCheckImport
-import {AppModuleNgFactory} from "./app.module.ngfactory";
-
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
